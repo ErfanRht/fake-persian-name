b3c3ca6b1987e3297e553aa9418411479f3a67b1
ErfanRht <erfanrht1384.com@gmail.com>
2021-09-11 11:29:55 +0430

    update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'fake_persian_name',
   packages = ['fake_persian_name'],
-  version = '1.1.0',
+  version = '1.1.1',
   license='MIT',
   description = 'A small Python library to generate fake Persian name.',
   author = 'Erfan Rahmati',
   author_email = 'ErfanRht2005@gmail.com',
   url = 'https://github.com/ErfanRht/fake-persian-name',
-  download_url = 'https://github.com/ErfanRht/fake_persian_name/blob/master/dist/fake_persian_name-1.1.0.tar.gz',
+  download_url = 'https://github.com/ErfanRht/fake_persian_name/blob/master/dist/fake_persian_name-1.1.1.tar.gz',
   keywords = ['fake', 'persian', 'name', 'persian name', 'fake name'],
   classifiers=[
     'Development Status :: 3 - Alpha',
@@ -20,7 +20,7 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
-  requires= [
+  install_requires= [
     "requests"
   ],
   include_package_data=True,
